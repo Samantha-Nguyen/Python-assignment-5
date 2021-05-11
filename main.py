eb94060f03634eb5c8ade72dd539917aeba2ae7f
SamanthaNguyen1 <>
2021-05-11 19:06:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import math
+
+d = input("What is the diameter of your circle?  ")
+area = 0
+circumference = 0
+pi = math.pi
+
+
+def is_digit(d):
+   try:
+    int(d)
+    return True
+   except ValueError:
+    return False
+
+
+def validate_input(d):
+  while (True):
+     if is_digit(d) == False:
+       d = input("Please enter a numerical value ")
+     elif (int(d) < 0):
+       d = input("Please enter a value greater than 0 ")
+     else:
+       return int(d)
+
+
+d = validate_input(d)
+
+area = math.pi * ((d / 2) * (d / 2))
+circumference = math.pi * d
+
+roundedArea = round(area, 2)
+roundedCircumference = round(circumference, 2)
+
+print('The area of your circle is ', roundedArea)
+print('The circumference of your cirlce is', roundedCircumference)
+print('Thank you for using this program')
