85b471fdbd75269c58f28b4d94937af019609919
SamanthaNguyen1 <>
2021-05-11 19:42:40 +0000

    Syntax errors

--- main.py
+++ main.py
@@ -3,30 +3,27 @@ import math
 d = input("What is the diameter of your circle?  ")
 area = 0
 circumference = 0
-pi = math.pi
-
 
 def is_digit(d):
-    try:
-        int(d)
-        return True
-    except ValueError:
-        return False
-
+  try:
+    int(d)
+    return True
+  except ValueError:
+    return False
 
 def validate_input(d):
-    while (True):
-        if is_digit(d) is False:
-            d = input("Please enter a numerical value")
-        elif (int(d) < 0):
-            d = input("Please enter a value greater than 0")
-        else:
-            return int(d)
-
+  while (True):
+    if is_digit(d) is False:
+      d = input("Please enter a numerical value")
+    elif (int(d) < 0):
+      d = input("Please enter a value greater than 0")
+    else:
+      return int(d)
+      break
 
 d = validate_input(d)
 
-area = math.pi * ((d / 2) * (d / 2))
+area = math.pi * ((d / 2)^2)
 circumference = math.pi * d
 
 roundedArea = round(area, 2)
